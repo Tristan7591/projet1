8566286fc0c3955312239fd2771c8144d6fab658
tristan <tristan13bellonie@gmail.com>
2025-03-22 20:35:08 +0100

    maj fichier d'eéétat

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,13 +1,12 @@
 terraform {
-  /*
   backend "s3" {
-    bucket         = "mon-bucket-terraform"   # À adapter
-    key            = "infra/eks-rds.tfstate"
-    region         = "us-east-1"              # À adapter
-    dynamodb_table = "terraform-lock-table"   # À adapter
+    bucket         = "digital-store-terraform-state"
+    key            = "state/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
     encrypt        = true
   }
-  */
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
